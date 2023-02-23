76efc3038f8a76cdcdac3d732600d02cf35df9d2
taberah <52472112+nonso7@users.noreply.github.com>
2023-02-23 05:14:51 -0800

    Delete question-1.py

--- Desktop/Assignment 1/question-1.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def nameValidator(name):
-
-        if re.match('[A-Za-z]{5,20}\s[A-Za-z]{5,20}', name):
-            print("Validation Successful")
-        else:
-                print("Validation Failed")   
-nameValidator("")                
