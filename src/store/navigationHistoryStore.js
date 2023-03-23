974953d1ac0d5a2fbe2f9dab2c503efeaf7f7e94
Caleb Uche <ebukauche52@gmail.com>
2023-03-23 01:29:08 +0100

    navigation history with zustand

---
+++ src/store/navigationHistoryStore.js
@@ -0,0 +1,25 @@
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+const navigationHistoryStore = create(
+  persist <Readonly<INavigationHistoryStore>> (
+    (set, get) => ({
+      history: [],
+      setHistory: (pathName) =>
+        set(() => ({ history: [...get().history, pathName] })),
+    }),
+    {
+      name: 'useNavigationHistoryStore',
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
+
+export const useNavigationHistoryStore = () => {
+  const store = navigationHistoryStore(({ history, setHistory }) => ({
+    history,
+    setHistory,
+  }))
+
+  return store
+};
