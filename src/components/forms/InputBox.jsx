c57a128c32294258b406d7f8b2c3b8666c6c9ac8
Caleb Uche <ebukauche52@gmail.com>
2023-03-24 15:13:26 +0100

    customer signup

--- src/components/forms/InputBox.jsx
+++ src/components/forms/InputBox.jsx
@@ -3,22 +3,23 @@ const InputBox = ({
   name,
   label,
   isCompulsory,
+  type = 'text',
   ...otherProps
 }) => {
   return (
-      <div className="font-notosans mt-6 ">
-        <label className="font-bold text-sm mb-2" htmlFor={name}>
-          {label}
-          {isCompulsory && <span className="text-red-500">*</span>}
-        </label>
-        <input
-          className="flex h-[43px] bg-white text-sm border rounded-[8px] border-ayaNeutral-600 w-[309px] outline-none px-6 placeholder:text-ayaNeutral-500 placeholder:text-sm "
-          type="text"
-          id={name}
-          name={name}
-          {...otherProps}
-        />
-      </div>
+    <div className="font-notosans mt-6 ">
+      <label className="text-sm text-ayaNeutral-900 font-normal mb-4" htmlFor={name}>
+        {label}
+        {isCompulsory && <span className="text-ayaDanger-200">*</span>}
+      </label>
+      <input
+        className="flex h-[54px] bg-white text-sm border rounded-[8px] border-ayaNeutral-600 w-[426px] outline-none px-6 placeholder:text-ayaNeutral-500 placeholder:text-sm "
+        type={type}
+        id={name}
+        name={name}
+        {...otherProps}
+      />
+    </div>
   )
 }
 
--- src/pages/auth/buyer/SignUp.jsx
+++ src/pages/auth/buyer/SignUp.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import AyaponeLogo from '../../../assets/images/ayapone_logo.svg'
 import InputBox from '../../../components/forms/InputBox'
+import { Link } from 'react-router-dom'
+import routes from '../../../navigation/routes'
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -8,14 +10,28 @@ const SignUp = () => {
   const handleMouseDownPassword = () => setShowPassword(!showPassword);
 
   return (
-    <div className='bg-ayaNeutral-100 h-auto min-h-screen w-screen grid place-items-center'>
-        <img src={AyaponeLogo} alt="logo" className='py-12'/>
+    <div className='bg-ayaNeutral-100 h-auto min-h-screen w-full grid place-items-center text-ayaNeutral-900'>
+        <img src={AyaponeLogo} alt="logo" className='py-12 scale-150'/>
 
-        <div className='bg-white rounded-[36px] min-h-[840px] min-w-[508px] py-16 px-10 shadow'>
-          <InputBox name={'name'} label={'Name'} isCompulsory={true} placeHolder={'First Name and Last Name'} />
-          <InputBox name={'email'} label={'Email Address'} isCompulsory={true} placeHolder={'Enter Email Address'} />
-          <InputBox name={'phone'} label={'Phone Number'} isCompulsory={true} placeHolder={'Enter Phone Number'} />
-          <InputBox name={'password'} label={'Password'} isCompulsory={true} placeHolder={'Enter Password'} />
+        <div className='bg-white rounded-[36px] h-auto w-[508px] py-16 px-10 mb-14 shadow'>
+          <div className="text-center mb-10">
+            <span className="font-bold text-2xl ">Sign Up</span><br />
+            <span className="text-base ">Create your Ayapone account</span>
+          </div>
+          <InputBox name={'name'} label={'Name'} isCompulsory={true} placeholder={'First Name and Last Name'} />
+          <InputBox name={'email'} label={'Email Address'} isCompulsory={true} placeholder={'Enter Email Address'} />
+          <InputBox name={'phone'} label={'Phone Number'} isCompulsory={true} placeholder={'Enter Phone Number'} />
+          <InputBox name={'password'} label={'Password'} isCompulsory={true} type={'password'} placeholder={'Enter Password'} />
+
+          <button type="submit" className='bg-ayaPrimary-600 font-bold rounded-[8px] mt-12 text-white w-[426px] h-[54px]'>
+            Sign Up
+          </button>
+
+          <div className="text-center mt-10">
+            <span className="text-base">Already have an account? <Link to={routes.BUYER_SIGN_IN_PAGE} className='font-bold text-ayaPrimary-600'>Log In</Link></span>
+            
+            <div className="text-lg pt-6">By signing up you accept our <Link to={routes.TERMS_AND_CONDITION_PAGE} className='text-ayaPrimary-600'>terms and condition & private policy</Link></div>
+          </div>
         </div>
     </div>
   );
