8683b5121501017b9b481ea660a80e9591483c82
Caleb Uche <ebukauche52@gmail.com>
2023-03-24 17:10:55 +0100

    merchant sign in

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,8 +9,12 @@ const HomePage = () => {
       <Link to={routes.BUYER_SIGN_UP_PAGE} className='bg-ayaPrimary-600 font-bold rounded-[8px] mt-12 text-white w-[200px] h-[54px]'>Buyer Sign Up</Link>
       <br /><br />
       <Link to={routes.BUYER_SIGN_IN_PAGE} className='bg-ayaPrimary-600 font-bold rounded-[8px] mt-12 text-white w-[200px] h-[54px]'>Buyer Sign In</Link>
+      <br /><br /><br /><br />
+      <Link to={routes.DASHBOARD_PAGE} className='bg-ayaPrimary-600 font-bold rounded-[8px] mt-12 text-white w-[200px] h-[54px]'>Dashboard</Link>
+      <br /><br /><br /><br /><br /><br />
+      <Link to={routes.MERCHANT_SIGN_IN_PAGE} className='bg-ayaPrimary-600 font-bold rounded-[8px] mt-12 text-white w-[200px] h-[54px]'>Merchant Sign In</Link>
       <br /><br />
-      <Link to={routes.DASHBOARD_PAGE} className='bg-ayaPrimary-600 font-bold rounded-[8px] mt-12 text-white w-[200px] h-[54px]'>Customer Dashboard</Link>
+      <Link to={routes.MERCHANT_SIGN_UP_PAGE} className='bg-ayaPrimary-600 font-bold rounded-[8px] mt-12 text-white w-[200px] h-[54px]'>Merchant Sign Up</Link>
     </main>
   );
 };
