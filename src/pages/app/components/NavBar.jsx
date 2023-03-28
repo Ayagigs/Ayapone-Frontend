8d419ce272ca09c79fcbcd9c1ab12ffdbc349133
Caleb Uche <ebukauche52@gmail.com>
2023-03-28 12:42:39 +0100

    z-index

--- src/pages/app/components/NavBar.jsx
+++ src/pages/app/components/NavBar.jsx
@@ -4,24 +4,25 @@ const NavBar = ({
   pageName
 }) => {
   return (
-    <header className='ml-1'>
-    <nav className="container flex bg-white rounded-bl-lg py-7 px-12 h-24 items-center fixed w-[calc(100vw-80px)] sm:w-[calc(100vw-16.5em)]">
-      <span className="text-2xl">{pageName}</span>
-      <ul className="hidden sm:flex ml-auto font-poppins text-base gap-x-6 items-center">
-        <li>
-          <img src={UserAvater} alt="User" srcSet={UserAvater} />
-        </li>
-        <li>
-          <input
-            className="flex h-[43px] bg-white text-sm border rounded-[16px] border-ayaNeutral-600 w-[309px] outline-none px-6 placeholder:text-ayaNeutral-500 placeholder:text-sm "
-            type="text"
-            id=""
-            name=""
-            placeholder='Search'
-          />
-        </li>
-      </ul>
-    </nav></header>
+    <header className='ml-1 z-[1000]'>
+      <nav className="container flex bg-white rounded-bl-lg py-7 px-12 h-24 items-center fixed w-[calc(100vw-80px)] sm:w-[calc(100vw-16.5em)]">
+        <span className="text-2xl">{pageName}</span>
+        <ul className="hidden sm:flex ml-auto font-poppins text-base gap-x-6 items-center">
+          <li>
+            <img src={UserAvater} alt="User" srcSet={UserAvater} />
+          </li>
+          <li>
+            <input
+              className="flex h-[43px] bg-white text-sm border rounded-[16px] border-ayaNeutral-600 w-[309px] outline-none px-6 placeholder:text-ayaNeutral-500 placeholder:text-sm "
+              type="text"
+              id=""
+              name=""
+              placeholder='Search'
+            />
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
